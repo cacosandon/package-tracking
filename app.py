1f006ab19baf662f0ce80068082c88cebad5b007
cacosandon <jiossandon@uc.cl>
2022-07-03 16:06:19 -0400

    chore: add app.py to expose api endpoint

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+from tracking import get_status_from_code
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+@app.route('/tracking/<code>', methods=['GET'])
+def get_tracking_status(code):
+    response = { 'message':  get_status_from_code(code) }
+
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(port=5000)
