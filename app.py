65825a472a971949fbbd3dba93f7923413a24604
cacosandon <jiossandon@uc.cl>
2022-10-04 20:01:59 -0300

    feat: add home page

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 CORS(app)
 
+@app.route('/')
+def index():
+    return 'Hello. Welcome to Modelandola Tracking API :)'
+
 @app.route('/tracking/<code>', methods=['GET'])
 def get_tracking_status(code):
     response = { 'message':  get_status_from_code(code) }
@@ -13,4 +17,4 @@ def get_tracking_status(code):
     return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run()
